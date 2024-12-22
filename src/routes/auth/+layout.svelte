663432f85773098d52cbffdb5e2cbdb52dcd7652
Jacob Singer <jacobleesinger@gmail.com>
2024-12-22 14:05:01 -0600

    feat: add supabase auth

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+	let { data, children }: { data: LayoutData, children: Snippet } = $props();
+
+</script>
+
+<header>
+	<nav>
+		<a href="/">Home</a>
+	</nav>
+</header>
+{@render children()}
